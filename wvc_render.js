1f68534b92099e4c2f4b16d554ea791a290946de
MixBadGun <1059129006@qq.com>
2024-01-05 21:52:15 +0800

    yes

--- wvc_render.js
+++ wvc_render.js
@@ -1,28 +1,34 @@
 import WebVideoCreator, { VIDEO_ENCODER, logger } from "web-video-creator";
 
-const src = process.argv.splice(2)[0]
-const data = require(src)
+const src = process.argv.splice(2)
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const data = require(src[0])
+
+const wvc = new WebVideoCreator();
 
 wvc.config({
-    mp4Encoder: VIDEO_ENCODER.NVIDIA.H264
+    mp4Encoder: VIDEO_ENCODER.INTEL.H264
 });
 
 // 创建单幕视频
 const video = wvc.createSingleVideo({
-    // 需要渲染的页面地址
-    url: data.url,
-    // 视频宽度
+    url: data.url + "?" + ,
     width: 1920,
-    // 视频高度
     height: 1080,
-    // 视频帧率
     fps: 60,
-    // 视频时长
     duration: data.full_time * 1000,
-    // 视频输出路径
     outputPath: data.output_src,
-    // 是否在cli显示进度条，默认是不显示
-    showProgress: true
+    // GPU 加速
+    browserUseGPU: true,
+    // Cli 的进度条
+    showProgress: true,
+    pagePrepareFn: async page => {
+        const _page = page.target;
+        await _page.evacuate(function(){
+            inject_wvc(data);
+        })
+    }
 });
 
 // 监听合成完成事件
