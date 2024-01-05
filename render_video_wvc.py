44be931f331255a77ad2c017bdb20a9615378b55
MixBadGun <1059129006@qq.com>
2024-01-05 19:01:31 +0800

    wvc

--- render_video_wvc.py
+++ render_video_wvc.py
@@ -1,7 +1,6 @@
-import execjs
+import subprocess
 import json
 
 def render_video(data,url,audio = None,fast = False):
-    with open('wvc_render.js','r') as render_js:
-        code = execjs.compile(render_js.read())
-        code.call('render_video',data)
+    command = ["node","wvc_render.js",json.dumps(data)]
+    subprocess.Popen(command).wait()
