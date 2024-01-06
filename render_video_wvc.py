08922c9cee6568c112534da6f9ac7108245253de
MixBadGun <1059129006@qq.com>
2024-01-06 12:21:24 +0800

    data

--- render_video_wvc.py
+++ render_video_wvc.py
@@ -9,10 +9,10 @@ def render_video(data,url,audio = None,fast = False):
     full_duration = data["full_time"]
     audio_file = audio_process(data["aid"],float(start_time)*1000,float(full_duration)*1000,audio)
     data.update({ "audio_src": audio_file })
-    
+
     identify_code = hashlib.md5(str(data).encode()).hexdigest()
     json_name = f"temp/{identify_code}.json"
     with open(json_name,"w",encoding="utf-8-sig") as temp_json:
-        json.dump(temp_json)
+        json.dump(data,temp_json)
     command = ["node","wvc_render.js",json_name]
     subprocess.Popen(command).wait()
