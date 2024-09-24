bb1dca8fa2720fb22c353919b6d07f86856900cc
MixBadGun <1059129006@qq.com>
2024-09-24 19:14:37 +0800

    logger fix

--- advanced_data_get.py
+++ advanced_data_get.py
@@ -1,11 +1,13 @@
 import logging
 import traceback
 
+
 def advanced_data_get():
     try:
         mainfunc()
     except Exception as e:
         logging.exception(traceback.format_exc())
+
 
 def mainfunc():
     """
@@ -13,19 +15,29 @@ def mainfunc():
     """
     from config import main_end, side_end, sep_time, web_prefix
 
-    logging.basicConfig(format='[%(levelname)s]\t%(message)s',level=logging.INFO, encoding="utf-8-sig")
-    formatter = logging.Formatter('[%(levelname)s]\t%(message)s')
+    formatter = logging.Formatter("[%(levelname)s]\t%(message)s")
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
-    console_handler.setLevel('INFO')
+    console_handler.setLevel("INFO")
     logger = logging.getLogger()
     logger.addHandler(console_handler)
 
     logging.info("开始获取数据...")
 
     import csv
-    from get_video_info_score import aid_to_score_norm, selected_video_stat, all_video_info
-    from program_function import check_dir , get_img , calc_color , exactVideoLength , html_unescape , get_danmaku
+    from get_video_info_score import (
+        aid_to_score_norm,
+        selected_video_stat,
+        all_video_info,
+    )
+    from program_function import (
+        check_dir,
+        get_img,
+        calc_color,
+        exactVideoLength,
+        html_unescape,
+        get_danmaku,
+    )
     from danmuku_time import danmuku_time
 
     # 新建不存在的文件夹
@@ -33,32 +45,53 @@ def mainfunc():
 
     # 预先黑名单
     blackArr = []
-    with open("option/blacklist.csv",encoding="utf-8-sig",newline='') as blackfile:
+    with open("option/blacklist.csv", encoding="utf-8-sig", newline="") as blackfile:
         blackInfo = csv.DictReader(blackfile)
         for bl in blackInfo:
             blackArr.append(int(bl["aid"]))
 
     # 预先内容系数调整
     adjust_dic = {}
-    with open("option/adjust.csv",encoding="utf-8-sig",newline='') as adjustfile:
+    with open("option/adjust.csv", encoding="utf-8-sig", newline="") as adjustfile:
         adjustInfo = csv.DictReader(adjustfile)
         for adj in adjustInfo:
             adjust_dic[int(adj["uid"])] = adj["adjust_scale"]
 
-    co_header = ['ranking','score',
-                'aid','bvid','cid',
-                'title','uploader',"uid",'copyright',
-                'play','like','coin','star',
-                'pubtime',
-                'adjust_scale','prescore',
-                'part','duration','start_time','full_time',
-                'web_prefix','video_src','cover_src','avatar_src','danmaku_src',
-                'light_color', 'dark_color', 'score_add']
+    co_header = [
+        "ranking",
+        "score",
+        "aid",
+        "bvid",
+        "cid",
+        "title",
+        "uploader",
+        "uid",
+        "copyright",
+        "play",
+        "like",
+        "coin",
+        "star",
+        "pubtime",
+        "adjust_scale",
+        "prescore",
+        "part",
+        "duration",
+        "start_time",
+        "full_time",
+        "web_prefix",
+        "video_src",
+        "cover_src",
+        "avatar_src",
+        "danmaku_src",
+        "light_color",
+        "dark_color",
+        "score_add",
+    ]
 
-    logging.info('生成 CSV 信息表格')
+    logging.info("生成 CSV 信息表格")
 
-    with open("./data/data.csv","w",encoding="utf-8-sig",newline='') as csvfile:
-        writer = csv.DictWriter(csvfile,co_header)
+    with open("./data/data.csv", "w", encoding="utf-8-sig", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, co_header)
         writer.writeheader()
         vid_list = []
         for video_aid, video_info in all_video_info.items():
@@ -67,74 +100,92 @@ def mainfunc():
                 continue
             normk = adjust_dic.get(int(video_info["mid"]), 1)
             prescore = aid_to_score_norm[video_aid]
-            norm_score = float('%.3f' % (prescore * float(normk)))
-            duration = video_stat.get("pages",[{"duration": 0}])[0]["duration"]
-            vid_list.append({
-                "prescore": prescore,
-                "score": norm_score,
-                "aid": str(video_aid),
-                "bvid": str(video_info["bvid"]),
-                "cid": str(video_stat.get("cid","未取得")),
-                "title": str(video_info["title"]),
-                "uploader": str(video_info["author"]),
-                "uid": str(video_info["mid"]),
-                "copyright": str(video_stat.get("copyright","未取得")),
-                "play": str(video_info["play"]),
-                "like": str(video_stat.get("like", "未取得")),
-                "coin": str(video_stat.get("coin", "未取得")),
-                "star": str(video_info["favorites"]),
-                "pubtime": str(video_info["pubdate"]),
-                "adjust_scale": str(normk),
-                "part": '1',
-                "duration": duration,
-                "start_time": '未取得',
-                "full_time": '未取得',
-                "web_prefix": '未取得',
-                "video_src": '未取得',
-                "avatar_src": '未取得',
-                "cover_src": '未取得', #str(video_info["pic"]),
-                "danmaku_src": '未取得',
-                "light_color": '未取得',
-                "dark_color": '未取得',
-                "score_add": ''
-            })
-        vid_list = sorted(vid_list,key=lambda x:float(x["score"]),reverse=True) # 排序
+            norm_score = float("%.3f" % (prescore * float(normk)))
+            duration = video_stat.get("pages", [{"duration": 0}])[0]["duration"]
+            vid_list.append(
+                {
+                    "prescore": prescore,
+                    "score": norm_score,
+                    "aid": str(video_aid),
+                    "bvid": str(video_info["bvid"]),
+                    "cid": str(video_stat.get("cid", "未取得")),
+                    "title": str(video_info["title"]),
+                    "uploader": str(video_info["author"]),
+                    "uid": str(video_info["mid"]),
+                    "copyright": str(video_stat.get("copyright", "未取得")),
+                    "play": str(video_info["play"]),
+                    "like": str(video_stat.get("like", "未取得")),
+                    "coin": str(video_stat.get("coin", "未取得")),
+                    "star": str(video_info["favorites"]),
+                    "pubtime": str(video_info["pubdate"]),
+                    "adjust_scale": str(normk),
+                    "part": "1",
+                    "duration": duration,
+                    "start_time": "未取得",
+                    "full_time": "未取得",
+                    "web_prefix": "未取得",
+                    "video_src": "未取得",
+                    "avatar_src": "未取得",
+                    "cover_src": "未取得",  # str(video_info["pic"]),
+                    "danmaku_src": "未取得",
+                    "light_color": "未取得",
+                    "dark_color": "未取得",
+                    "score_add": "",
+                }
+            )
+        vid_list = sorted(
+            vid_list, key=lambda x: float(x["score"]), reverse=True
+        )  # 排序
         ranking = 0
         ranked_list = []
         for vid in vid_list:
             vid = html_unescape(vid)
             ranking += 1
-            after_dict = { "ranking": ranking }
+            after_dict = {"ranking": ranking}
             if ranking <= main_end + side_end + 15:
                 try:
-                    pic_src = get_img(vid["aid"]) # 存在获取失败的可能，于是可以选择跳过。
+                    pic_src = get_img(
+                        vid["aid"]
+                    )  # 存在获取失败的可能，于是可以选择跳过。
                 except:
                     ranking -= 1
                     continue
                 color_rgb = calc_color(pic_src["cover"])
-                after_dict.update({
-                    "avatar_src": pic_src["avatar"],
-                    "cover_src": pic_src["cover"],
-                    "light_color": str(color_rgb[0]),
-                    "dark_color": str(color_rgb[1]),
-                    "web_prefix": web_prefix
-                    })
+                after_dict.update(
+                    {
+                        "avatar_src": pic_src["avatar"],
+                        "cover_src": pic_src["cover"],
+                        "light_color": str(color_rgb[0]),
+                        "dark_color": str(color_rgb[1]),
+                        "web_prefix": web_prefix,
+                    }
+                )
                 if ranking <= main_end + 5:
-                    vid_src = f"./video/{vid['aid']}.mp4" # get_video(vid["aid"]) 不再在这个时候下载
-                    danmaku_src = get_danmaku(vid["cid"],aid=vid["aid"]) # 弹幕获取
-                    exact_time = float(vid["duration"]) # exactVideoLength(vid_src)
+                    vid_src = f"./video/{vid['aid']}.mp4"  # get_video(vid["aid"]) 不再在这个时候下载
+                    danmaku_src = get_danmaku(vid["cid"], aid=vid["aid"])  # 弹幕获取
+                    exact_time = float(vid["duration"])  # exactVideoLength(vid_src)
                     full = False
                     if ranking == 1:
                         full = True
-                    start_time , full_time = danmuku_time(vid["aid"],exact_time,sep_time,full=full,cid=vid["cid"])
-                    after_dict.update({ "video_src" : vid_src,
-                                        "duration" : exact_time,
-                                        "start_time": start_time,
-                                        "full_time" : full_time})
+                    start_time, full_time = danmuku_time(
+                        vid["aid"], exact_time, sep_time, full=full, cid=vid["cid"]
+                    )
+                    after_dict.update(
+                        {
+                            "video_src": vid_src,
+                            "duration": exact_time,
+                            "start_time": start_time,
+                            "full_time": full_time,
+                        }
+                    )
             vid.update(after_dict)
             ranked_list.append(vid)
         writer.writerows(ranked_list)
-    logging.info('周刊数据获取已全部完成！')
+    logging.info("周刊数据获取已全部完成！")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format="[%(levelname)s]\t%(message)s", level=logging.DEBUG, encoding="utf-8-sig"
+    )
     advanced_data_get()
--- main_progress.py
+++ main_progress.py
@@ -5,10 +5,11 @@ import yaml
 import time
 
 from all_create import AllVideo
-from program_function import convert_csv , extract_single_column , check_env , check_dir
+from program_function import convert_csv, extract_single_column, check_env, check_dir
 from render_video_wvc import render_video
 
 import traceback
+
 
 def main_progress():
     try:
@@ -16,14 +17,15 @@ def main_progress():
     except Exception as e:
         logging.exception(traceback.format_exc())
 
+
 def mainfunc():
     from config import render_prefix, web_prefix, main_end, side_end, muitl_limit
+
     # 日志记录
-    logging.basicConfig(format='[%(levelname)s]\t%(message)s',filename="log/" + time.strftime("%Y-%m-%d %H-%M-%S") + '.log', level=logging.INFO)
-    formatter = logging.Formatter('[%(levelname)s]\t%(message)s')
+    formatter = logging.Formatter("[%(levelname)s]\t%(message)s")
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
-    console_handler.setLevel('INFO')
+    console_handler.setLevel("INFO")
     logger = logging.getLogger()
     logger.addHandler(console_handler)
 
@@ -35,24 +37,24 @@ def mainfunc():
     calendar_list = convert_csv("./option/calendar.csv")
 
     # 日历合成
-    with open("./config/calendar.yaml","r") as conf_file:
+    with open("./config/calendar.yaml", "r") as conf_file:
         conf = yaml.safe_load(conf_file)
 
     if not os.path.exists(f"./output/clip/Calendar.mp4"):
         url = f"{render_prefix}/calendar"
         calendar_data = {
-                    "aid": "calendar",
-                    "output_src": f"./output/clip/Calendar.mp4",
-                    "full_time": conf["full_time"],
-                    "start_time": conf["start_time"],
-                    "web_prefix": web_prefix,
-                    "more_data": calendar_list,
-                    "url": url
-                }
-        render_video(calendar_data,url,fast = True,audio = "./option/calendar/bgm.mp3")
+            "aid": "calendar",
+            "output_src": f"./output/clip/Calendar.mp4",
+            "full_time": conf["full_time"],
+            "start_time": conf["start_time"],
+            "web_prefix": web_prefix,
+            "more_data": calendar_list,
+            "url": url,
+        }
+        render_video(calendar_data, url, fast=True, audio="./option/calendar/bgm.mp3")
 
     # 主榜列表单独提取
-    mainArr = extract_single_column(ranked_list,"aid",main_end)
+    mainArr = extract_single_column(ranked_list, "aid", main_end)
 
     # PICK UP 数据
     picked_list = []
@@ -71,24 +73,27 @@ def mainfunc():
 
         if render_times == 1:
             url = f"{render_prefix}/main"
-            viding.update({
-                "output_src": f"./output/clip/MainRank_1.mp4",
-                "side_duration": int(float(viding["full_time"]) * 0.6),
-                "more_data": ranked_list[main_end:main_end+side_end],
-                "url": url
-            })
+            viding.update(
+                {
+                    "output_src": f"./output/clip/MainRank_1.mp4",
+                    "side_duration": int(float(viding["full_time"]) * 0.6),
+                    "more_data": ranked_list[main_end : main_end + side_end],
+                    "url": url,
+                }
+            )
             muitl_limit.acquire()
-            rend_s = threading.Thread(target=render_video,args=(viding,url))
+            rend_s = threading.Thread(target=render_video, args=(viding, url))
             rend_s.start()
             rend_q.append(rend_s)
             # render_video(viding,url)
             continue
         # 否则正常渲染。
         url = f"{render_prefix}/main"
-        viding.update({ "output_src": f"./output/clip/MainRank_{render_times}.mp4", 
-                        "url": url})
+        viding.update(
+            {"output_src": f"./output/clip/MainRank_{render_times}.mp4", "url": url}
+        )
         muitl_limit.acquire()
-        rend_s = threading.Thread(target=render_video,args=(viding,url))
+        rend_s = threading.Thread(target=render_video, args=(viding, url))
         rend_s.start()
         rend_q.append(rend_s)
         # render_video(viding,url)
@@ -100,10 +105,11 @@ def mainfunc():
         url = f"{render_prefix}/pick"
         if os.path.exists(f"./output/clip/PickRank_{picks}.mp4"):
             continue
-        picking.update({ "output_src": f"./output/clip/PickRank_{picks}.mp4" ,
-                        "url": url})
+        picking.update(
+            {"output_src": f"./output/clip/PickRank_{picks}.mp4", "url": url}
+        )
         muitl_limit.acquire()
-        rend_s = threading.Thread(target=render_video,args=(picking,url))
+        rend_s = threading.Thread(target=render_video, args=(picking, url))
         rend_s.start()
         rend_q.append(rend_s)
         # render_video(picking,url)
@@ -112,9 +118,15 @@ def mainfunc():
         sq.join()
 
     # 总拼接
-    AllVideo(main_end,picked_list)
+    AllVideo(main_end, picked_list)
 
     logging.info("渲染工作全部完成！")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format="[%(levelname)s]\t%(message)s",
+        filename="log/" + time.strftime("%Y-%m-%d %H-%M-%S") + ".log",
+        level=logging.DEBUG,
+    )
     main_progress()
